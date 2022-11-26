290167381e80ca3f214148d7fced6e5f318fb579
Yifan <yifan.bao@influxdata.nz>
2022-11-26 20:41:41 +1300

    done with hello

---
+++ RandomWord.java
@@ -0,0 +1,11 @@
+package hello;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class RandomWord {
+    public static void main(String[] args) {
+
+        String s = StdIn.readString();
+        System.out.println(s);
+    }
+}
---
+++ hello/RandomWord.java
@@ -0,0 +1,8 @@
+import edu.princeton.cs.algs4.StdIn;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String s = StdIn.readString();
+        System.out.println(s);
+    }
+}
