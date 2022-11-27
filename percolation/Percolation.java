4abde234e8fa9057090805bba35ef86138e3067e
Yifan <yifan.bao@influxdata.nz>
2022-11-27 18:40:46 +1300

    basic function done

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,43 +1,104 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
 
     private boolean[] grid;
-    private int size;
+    private int size, numberOfOpenSites;
+    private WeightedQuickUnionUF full, prec;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         if (n < 0) throw new IllegalArgumentException();
         size = n;
         grid = new boolean[n * n];
+        full = new WeightedQuickUnionUF(n * n);
+        prec = new WeightedQuickUnionUF(n * n);
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        grid[getPos(row, col)] = true;
+        validation(row, col);
+        if (isOpen(row, col)) {
+            return;
+        }
+        grid[posToIdx(row, col)] = true;
+        numberOfOpenSites++;
+        checkAroundAndConnect(row, col);
+    }
+
+    private void checkAroundAndConnect(int row, int col) {
+        if (!isValidRowCol(row, col)) {
+            return;
+        }
+
+        if (row == 1) {
+            prec.union(0, posToIdx(row, col));
+            full.union(0, posToIdx(row, col));
+        }
+
+        if (row == size) {
+            prec.union(size * size - 1, posToIdx(row, col));
+        }
+
+        if (isValidBlk(row - 1, col)) {
+            full.union(posToIdx(row - 1, col), posToIdx(row, col));
+            prec.union(posToIdx(row - 1, col), posToIdx(row, col));
+        }
+
+        if (isValidBlk(row + 1, col)) {
+            full.union(posToIdx(row + 1, col), posToIdx(row, col));
+            prec.union(posToIdx(row + 1, col), posToIdx(row, col));
+        }
+
+        if (isValidBlk(row, col - 1)) {
+            full.union(posToIdx(row, col - 1), posToIdx(row, col));
+            prec.union(posToIdx(row, col - 1), posToIdx(row, col));
+        }
+
+        if (isValidBlk(row, col + 1)) {
+            full.union(posToIdx(row, col + 1), posToIdx(row, col));
+            prec.union(posToIdx(row, col + 1), posToIdx(row, col));
+        }
+
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return grid[getPos(row, col)];
+        return grid[posToIdx(row, col)];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return false;
+        return full.find(posToIdx(row, col)) == full.find(1);
     }
 
-    private int getPos(int row, int col) {
+    private int posToIdx(int row, int col) {
         return (row - 1) * size + (col - 1);
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return 0;
+        return numberOfOpenSites;
+    }
+
+    private boolean isValidRowCol(int row, int col) {
+        return row > 0 && row <= size && col > 0 && col <= size;
+    }
+
+    private boolean isValidBlk(int row, int col) {
+        return isValidRowCol(row, col) && isOpen(row, col);
+    }
+
+    private void validation(int row, int col) {
+        if (!isValidRowCol(row, col)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     // does the system percolate?
     public boolean percolates() {
         // when upper and lower connected
-        return false;
+        return full.find(0) == prec.find(size * size - 1);
     }
 
 
