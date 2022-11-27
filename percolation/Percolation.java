73e4de90449c07a1b61b2271313cc499054c6aa8
Yifan <yifan.bao@influxdata.nz>
2022-11-27 16:43:26 +1300

    make blocked grid

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,26 +1,32 @@
 public class Percolation {
 
+    private boolean[] grid;
+    private int size;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException();
-        }
+        if (n < 0) throw new IllegalArgumentException();
+        size = n;
+        grid = new boolean[n * n];
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-
+        grid[getPos(row, col)] = true;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        return grid[getPos(row, col)];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         return false;
+    }
+
+    private int getPos(int row, int col) {
+        return (row - 1) * size + (col - 1);
     }
 
     // returns the number of open sites
