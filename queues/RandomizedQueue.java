0457208a1b231c28688fbfe5c7095f0461039979
Yifan <yifan.bao@influxdata.nz>
2022-11-29 23:39:57 +1300

    function with resize done

--- queues/RandomizedQueue.java
+++ queues/RandomizedQueue.java
@@ -1,48 +1,104 @@
 /* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
+ *  Name: Yifan
+ *  Date: 2022-Nov-29
+ *  Description: Random queue with array
  **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private static final int INIT_CAPACITY = 4;
+    private Item[] elements;
+    private int size = 0;
+
 
     // construct an empty randomized queue
     public RandomizedQueue() {
+        elements = (Item[]) new Object[INIT_CAPACITY];
     }
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == elements.length;
     }
 
     // return the number of items on the randomized queue
     public int size() {
-        return 0;
+        return size;
     }
 
     // add the item
     public void enqueue(Item item) {
+        if (isFull()) {
+            resize(elements.length * 2);
+        }
+        elements[size++] = item;
     }
 
     // remove and return a random item
     public Item dequeue() {
-        return null;
+        if (isEmpty()) throw new java.util.NoSuchElementException();
+
+        int randIdx = StdRandom.uniformInt(size);
+        Item ele = sample(randIdx);
+        elements[randIdx] = elements[size - 1];
+        size--;
+        if (size > 0 && size == elements.length / 4) resize(elements.length / 2);
+        return ele;
     }
 
     // return a random item (but do not remove it)
-    public Item sample() {
-        return null;
+    public Item sample(int idx) {
+        if (isEmpty()) throw new java.util.NoSuchElementException();
+        return elements[idx];
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < size; i++)
+            copy[i] = elements[i];
+        elements = copy;
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-        return null;
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+
+        public boolean hasNext() {
+            return size > 0;
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new java.util.NoSuchElementException();
+            Item ele = dequeue();
+            return ele;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 
     // unit testing (required)
     public static void main(String[] args) {
+        RandomizedQueue<Integer> rqueue = new RandomizedQueue<Integer>();
+        for (int i = 1; i < 20; i++) {
+            rqueue.enqueue(i);
+        }
+        
+        for (int elt : rqueue) {
+            StdOut.print(elt + " ");
+        }
     }
 
 }
