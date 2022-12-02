d7ec2006b086a00df0f64daf2ed6384d77cf4b64
Yifan <bbbbb8bbbbb@gmail.com>
2022-12-02 17:16:17 +1300

    test line drawing

--- collinear/Point.java
+++ collinear/Point.java
@@ -8,9 +8,7 @@
  *
  ******************************************************************************/
 
-import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Comparator;
 
@@ -138,39 +136,9 @@ public class Point implements Comparable<Point> {
      */
     public static void main(String[] args) {
         /* YOUR CODE HERE */
-        // read the n points from a file
-        In in = new In(args[0]);
-        int n = in.readInt();
-        Point[] points = new Point[n];
-        for (int i = 0; i < n; i++) {
-            int x = in.readInt();
-            int y = in.readInt();
-            points[i] = new Point(x, y);
-        }
-
-        // draw the points
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(0, 32768);
-        StdDraw.setYscale(0, 32768);
-        for (Point p : points) {
-            p.draw();
-        }
-        StdDraw.show();
-
-        // print and draw the line segments
-        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
-        for (LineSegment segment : collinear.segments()) {
-            StdOut.println(segment);
-            segment.draw();
-        }
-        StdDraw.show();
-
-        // // print and draw the line segments
-        // FastCollinearPoints collinear = new FastCollinearPoints(points);
-        // for (LineSegment segment : collinear.segments()) {
-        //     StdOut.println(segment);
-        //     segment.draw();
-        // }
-        // StdDraw.show();
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(123123, 123123);
+        LineSegment line = new LineSegment(p1, p2);
+        line.draw();
     }
 }
