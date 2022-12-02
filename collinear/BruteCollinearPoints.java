d5f273be0b3b2d9070422bd1294604f49308fc88
Yifan <bbbbb8bbbbb@gmail.com>
2022-12-02 17:10:52 +1300

    skeleton for BruteCollinearPoints

---
+++ collinear/BruteCollinearPoints.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name: Yifan
+ *  Date: 2022-Dec-01
+ *  Description:
+ **************************************************************************** */
+
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) // finds all line segments containing 4 points
+    {
+
+    }
+
+    public int numberOfSegments()     // the number of line segments
+    {
+        return 0;
+    }
+
+    public LineSegment[] segments()       // the line segments
+    {
+        return new LineSegment[0];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
