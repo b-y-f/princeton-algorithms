52c7dde68cef7e0c91468358f5cff5e81d81b782
Yifan <bbbbb8bbbbb@gmail.com>
2022-12-04 23:44:45 +1300

    remove duplicated goal board, change formula for hamming

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -13,26 +13,15 @@ import java.util.Arrays;
 
 public class Board {
 
-    private int[][] board, goal;
+    private int[][] board;
+    private int dimension;
     private int hamming = 0, manhattan = 0;
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
         board = tiles.clone();
-        genGoal();
-    }
-
-    private void genGoal() {
-        goal = new int[dimension()][dimension()];
-        int tmp = 1;
-        for (int i = 0; i < dimension(); i++) {
-            for (int j = 0; j < dimension(); j++) {
-                goal[i][j] = tmp;
-                tmp++;
-            }
-        }
-        goal[dimension() - 1][dimension() - 1] = 0;
+        dimension = board.length;
     }
 
     // string representation of this board
@@ -50,20 +39,18 @@ public class Board {
 
     // board dimension n
     public int dimension() {
-        return board.length;
+        return dimension;
     }
 
     // number of tiles out of place
     public int hamming() {
+        int tmp = 1;
         for (int i = 0; i < dimension(); i++) {
             for (int j = 0; j < dimension(); j++) {
-                if (board[i][j] != 0 && board[i][j] != goal[i][j]) {
+                if (board[i][j] != 0 && board[i][j] != tmp) {
                     hamming++;
-                    int[] goalIdxs = getGoalIndex(board[i][j]);
-                    int gi = goalIdxs[0];
-                    int gj = goalIdxs[1];
-                    manhattan += Math.abs(i - gi) + Math.abs(j - gj);
                 }
+                tmp++;
             }
         }
         return hamming;
@@ -89,7 +76,7 @@ public class Board {
 
     // is this board the goal board?
     public boolean isGoal() {
-        return Arrays.deepEquals(board, goal);
+        return hamming() == 0;
     }
 
     // does this board equal y?
