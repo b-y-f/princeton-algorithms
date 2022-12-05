35e0e3fd0e4c43bb117832d5f20924ec58a32b7c
Yifan <bbbbb8bbbbb@gmail.com>
2022-12-05 14:04:21 +1300

    fixed structure

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -1,13 +1,10 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
-
 public class Solver {
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        if (initial == null) {
+            throw new IllegalArgumentException();
+        }
 
     }
 
@@ -18,11 +15,17 @@ public class Solver {
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
+        if (!isSolvable()) {
+            return -1;
+        }
         return 0;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
+        if (!isSolvable()) {
+            return null;
+        }
         return null;
     }
 
