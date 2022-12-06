0c9d90dd13ad01366a9af03e01e0dfc8738bb96d
Yifan <bbbbb8bbbbb@gmail.com>
2022-12-06 22:52:22 +1300

    add skeleton

---
+++ kdtree/KdTree.java
@@ -0,0 +1,22 @@
+/* *****************************************************************************
+ *  Name: Yifan
+ *  Date: 6/12/22
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Point2D;
+
+public class KdTree {
+    public static void main(String[] args) {
+
+    }
+
+    public void insert(Point2D p) {
+    }
+
+    public void draw() {
+    }
+
+    public Point2D nearest(Point2D query) {
+        return query;
+    }
+}
