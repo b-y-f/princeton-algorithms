7d2d0dbe0fe13a09774f9b28d68d241b394bb3d5
Yifan <bbbbb8bbbbb@gmail.com>
2022-12-08 00:24:37 +1300

    done with brutal force

--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -6,10 +6,11 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.Stack;
 
 // TODO You must use either SET or java.util.TreeSet; do not implement your own red–black BST.
 public class PointSET {
-    SET<Point2D> points;
+    private SET<Point2D> points;
 
     public PointSET()
     // construct an empty set of points
@@ -55,13 +56,25 @@ public class PointSET {
     public Iterable<Point2D> range(RectHV rect)
     // all points that are inside the rectangle (or on the boundary)
     {
-        return null;
+        Stack<Point2D> insiders = new Stack<Point2D>();
+        for (Point2D p : points) {
+            if (rect.contains(p)) {
+                insiders.push(p);
+            }
+        }
+        return insiders;
     }
 
     public Point2D nearest(Point2D p)
     // a nearest neighbor in the set to point p; null if the set is empty
     {
-        return null;
+        Point2D nearest = null;
+        double closetDist = Double.POSITIVE_INFINITY;
+        for (Point2D point : points) {
+            if (p.distanceTo(point) < closetDist)
+                nearest = point;
+        }
+        return nearest;
     }
 
     public static void main(String[] args) {
