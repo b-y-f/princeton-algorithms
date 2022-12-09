ca994e79cdd2876ac1abe5c3e32401fa75d217b0
Yifan <bbbbb8bbbbb@gmail.com>
2022-12-09 13:06:20 +1300

    done with insert func

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -8,11 +8,49 @@ import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
 
-    public KdTree() {
+    private KDNode root;
 
+    public static void main(String[] args) {
+        KdTree t = new KdTree();
+        for (int i = 0; i < 100; i++) {
+            double x = Math.random();
+            double y = Math.random();
+            t.insert(new Point2D(x, y));
+        }
+        System.out.println("done");
+    }
+
+    public KdTree() {
     }
 
     public void insert(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException();
+        }
+        root = insertRecur(p, root, 1);
+    }
+
+    private KDNode insertRecur(Point2D p, KDNode node, int level) {
+        if (node == null) {
+            node = new KDNode(p);
+        }
+        else if (getCutdim(p, level) < getCutdim(node.point, level)) {
+            node.left = insertRecur(p, node.left, level + 1);
+        }
+        else {
+            node.right = insertRecur(p, node.right, level + 1);
+        }
+        return node;
+    }
+
+    private double getCutdim(Point2D p, int level) {
+        boolean isX = level % 2 == 1;
+        if (isX) {
+            return p.x();
+        }
+        else {
+            return p.y();
+        }
     }
 
     public void draw() {
