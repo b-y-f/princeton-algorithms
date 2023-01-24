e7fa560eec979c4ee7a9534cdc47bd871c9c607f
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-24 18:38:41 +1300

    add a test for WordNet

---
+++ wordnet/WordNetTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WordNetTest {
+    @Test
+    public void pairsOfNounsAreVeryFarApart() {
+        WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
+        int sca = wordnet.distance("white_marlin", "mileage");
+        assertEquals(23, sca);
+        
+        int sca1 = wordnet.distance("Black_Plague", "black_marlin");
+        assertEquals(33, sca1);
+
+        int sca2 = wordnet.distance("Brown_Swiss", "barrel_roll");
+        assertEquals(29, sca2);
+    }
+
+}
