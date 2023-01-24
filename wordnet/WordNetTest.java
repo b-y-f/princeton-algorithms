d8dfeacade766db362b1e6f4c1392c33a485757f
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-24 23:26:15 +1300

    added new test basic on my last submit

--- wordnet/WordNetTest.java
+++ wordnet/WordNetTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class WordNetTest {
     @Test
@@ -25,4 +26,95 @@ class WordNetTest {
         }
         assertEquals(119188, cnt);
     }
+
+    @Test
+    public void distanceWithRandomNounPairs() {
+        WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
+        String nounA = "categorization";
+        String nounB = "Evenk";
+
+        assertEquals(12, wordnet.distance(nounA, nounB));
+    }
+
+    @Test
+    public void distanceWithAllNounPairsA() {
+        WordNet wordnet = new WordNet("synsets11.txt", "hypernyms11AmbiguousAncestor.txt");
+        String nounA = "a";
+        String nounB = "g";
+
+        assertEquals(4, wordnet.distance(nounA, nounB));
+    }
+
+    @Test
+    public void distanceWithAllNounPairsB() {
+        WordNet wordnet = new WordNet("synsets8.txt", "hypernyms8ModTree.txt");
+        String nounA = "f";
+        String nounB = "h";
+
+        assertEquals(2, wordnet.distance(nounA, nounB));
+    }
+
+    @Test
+    public void distanceWithAllNounPairsC() {
+        WordNet wordnet = new WordNet("synsets11.txt", "hypernyms11ManyPathsOneAncestor.txt");
+        String nounA = "a";
+        String nounB = "c";
+
+        assertEquals(1, wordnet.distance(nounA, nounB));
+    }
+
+    @Test
+    public void distanceWithAllNounPairsD() {
+        WordNet wordnet = new WordNet("synsets8.txt", "hypernyms8WrongBFS.txt");
+        String nounA = "a";
+        String nounB = "d";
+
+        assertEquals(2, wordnet.distance(nounA, nounB));
+    }
+
+
+    @Test
+    public void distanceWithRandPairsA() {
+        WordNet wordnet = new WordNet("synsets100-subgraph.txt", "hypernyms100-subgraph.txt");
+        String nounA = "IgG";
+        String nounB = "ricin_toxin";
+        assertEquals(6, wordnet.distance(nounA, nounB));
+    }
+
+    @Test
+    public void distanceWithRandPairsB() {
+        WordNet wordnet = new WordNet("synsets500-subgraph..txt", "hypernyms500-subgraph.txt");
+        String nounA = "rennin";
+        String nounB = "linalool";
+        assertEquals(7, wordnet.distance(nounA, nounB));
+    }
+
+    @Test
+    public void distanceWithRandPairsC() {
+        WordNet wordnet = new WordNet("synsets1000-subgraph.txt", "hypernyms1000-subgraph.txt");
+        String nounA = "trigeminal";
+        String nounB = "chymosin";
+        assertEquals(15, wordnet.distance(nounA, nounB));
+    }
+
+    @Test
+    public void sapWithRandomNounPairs() {
+        WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
+        String nounA = "filter_paper";
+        String nounB = "bioclimatology";
+        assertEquals(17, wordnet.distance(nounA, nounB));
+        assertEquals("abstraction abstract_entity", wordnet.sap(nounA, nounB));
+    }
+
+    @Test
+    public void constructorWhenInputIsNotRootedDAG() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            WordNet wordnet = new WordNet("synsets3.txt", "hypernyms3InvalidCycle.txt");
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            WordNet wordnet = new WordNet("synsets6.txt", "hypernyms6InvalidCycle.txt");
+        });
+    }
+
 }
