51e39292977bcef1cb6061d3e7e728b22560085c
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-24 19:17:17 +1300

    add few more tests

--- wordnet/WordNetTest.java
+++ wordnet/WordNetTest.java
@@ -8,7 +8,7 @@ class WordNetTest {
         WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
         int sca = wordnet.distance("white_marlin", "mileage");
         assertEquals(23, sca);
-        
+
         int sca1 = wordnet.distance("Black_Plague", "black_marlin");
         assertEquals(33, sca1);
 
@@ -16,4 +16,52 @@ class WordNetTest {
         assertEquals(29, sca2);
     }
 
+    @Test
+    public void testTotalNumberOfNouns() {
+        WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
+        int cnt = 0;
+        for (String ignored : wordnet.nouns()) {
+            cnt++;
+        }
+        assertEquals(119188, cnt);
+    }
+
+    /**
+     * there are 5 IDs associated with worm
+     * 81679 worm
+     * 81680 worm
+     * 81681 worm
+     * 81682 worm louse insect dirt_ball
+     */
+    @Test
+    public void containWorm() {
+        WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
+        int cnt = 0;
+        for (String s : wordnet.nouns()) {
+            if (s.equals("worm")) {
+                cnt++;
+            }
+        }
+        assertEquals(5, cnt);
+    }
+
+    @Test
+    public void containBird() {
+        WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
+        int cnt = 0;
+        for (String s : wordnet.nouns()) {
+            if (s.equals("bird")) {
+                cnt++;
+            }
+        }
+        assertEquals(5, cnt);
+    }
+
+    @Test
+    public void validateEdgeAndVertex() {
+        WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
+        SAP sap = wordnet.getSAP();
+        assertEquals(82192, sap.getNumOfVertex());
+        assertEquals(84505, sap.getNumOfEdges());
+    }
 }
