dfef66bc8929b5a1cfdae2a785580fe65f521308
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-31 19:27:05 +1300

    Refactored

--- seam/SeamCarver.java
+++ seam/SeamCarver.java
@@ -3,46 +3,48 @@ import edu.princeton.cs.algs4.Picture;
 import java.awt.Color;
 
 public class SeamCarver {
+    private static final int DEFAULT_ENERGY = 1000;
     private Picture pic;
     private double[][] energy;
 
-    private final int defaultEnergy = 1000;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
         pic = picture;
-        energy = new double[height()][width()];
-        for (int i = 0; i < height(); i++) {
-            for (int j = 0; j < width(); j++) {
-                initEnergy(i, j);
-            }
-        }
+        initEnergy();
 
     }
 
-    private void initEnergy(int i, int j) {
-        if (i == 0)
-            energy[i][j] = defaultEnergy;
-        else if (i == height() - 1)
-            energy[i][j] = defaultEnergy;
-        else if (j == 0)
-            energy[i][j] = defaultEnergy;
-        else if (j == width() - 1)
-            energy[i][j] = defaultEnergy;
-        else {
-            Color left = pic.get(j - 1, i);
-            Color right = pic.get(j + 1, i);
-            Color up = pic.get(j, i - 1);
-            Color down = pic.get(j, i + 1);
-            double deltaX = Math.pow(right.getRed() - left.getRed(), 2) + Math.pow(
-                    right.getGreen() - left.getGreen(), 2) + Math.pow(
-                    right.getBlue() - left.getBlue(), 2);
-            double deltaY = Math.pow(up.getRed() - down.getRed(), 2) + Math.pow(
-                    up.getGreen() - down.getGreen(), 2) + Math.pow(
-                    up.getBlue() - down.getBlue(), 2);
+    private void initEnergy() {
+        energy = new double[height()][width()];
+        for (int i = 0; i < height(); i++) {
+            for (int j = 0; j < width(); j++) {
+                setEnergyAt(i, j);
+            }
+        }
+    }
 
+    private void setEnergyAt(int i, int j) {
+        if (i == 0)
+            energy[i][j] = DEFAULT_ENERGY;
+        else if (i == height() - 1)
+            energy[i][j] = DEFAULT_ENERGY;
+        else if (j == 0)
+            energy[i][j] = DEFAULT_ENERGY;
+        else if (j == width() - 1)
+            energy[i][j] = DEFAULT_ENERGY;
+        else {
+            double deltaX = getDelta(pic.get(j - 1, i), pic.get(j + 1, i));
+            double deltaY = getDelta(pic.get(j, i + 1), pic.get(j, i - 1));
             energy[i][j] = Math.sqrt(deltaX + deltaY);
         }
+    }
+
+    private static double getDelta(Color left, Color right) {
+        double deltaX = Math.pow(right.getRed() - left.getRed(), 2) + Math.pow(
+                right.getGreen() - left.getGreen(), 2) + Math.pow(
+                right.getBlue() - left.getBlue(), 2);
+        return deltaX;
     }
 
 
