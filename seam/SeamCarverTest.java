35846e9c0609c5b2785f9f15c18b4579cab313f3
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-31 19:51:27 +1300

    AddTest: find Seam

--- seam/SeamCarverTest.java
+++ seam/SeamCarverTest.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.Picture;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -16,6 +17,24 @@ class SeamCarverTest {
         // boarder should be 1000
         assertEquals(1000, sc.energy(0, 0));
         assertEquals(1000, sc.energy(2, 2));
+    }
+
+    @Test
+    public void testVerticalSeam() {
+        Picture picture = new Picture("6x5.png");
+        SeamCarver sc = new SeamCarver(picture);
+
+        int[] exp = new int[] { 3, 4, 3, 2, 2 };
+        assertArrayEquals(exp, sc.findVerticalSeam());
+    }
+
+    @Test
+    public void testHorizontalSeam() {
+        Picture picture = new Picture("6x5.png");
+        SeamCarver sc = new SeamCarver(picture);
+
+        int[] exp = new int[] { 2, 2, 1, 2, 1, 2 };
+        assertArrayEquals(exp, sc.findHorizontalSeam());
     }
 
 }
