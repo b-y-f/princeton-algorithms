9aae27d73b66c8675e1d184e895caaae0dd5ce84
Yifan <bbbbb8bbbbb@gmail.com>
2023-01-31 19:17:16 +1300

    PassTest: energy initial

--- seam/SeamCarver.java
+++ seam/SeamCarver.java
@@ -1,29 +1,69 @@
 import edu.princeton.cs.algs4.Picture;
 
+import java.awt.Color;
+
 public class SeamCarver {
+    private Picture pic;
+    private double[][] energy;
+
+    private final int defaultEnergy = 1000;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        pic = picture;
+        energy = new double[height()][width()];
+        for (int i = 0; i < height(); i++) {
+            for (int j = 0; j < width(); j++) {
+                initEnergy(i, j);
+            }
+        }
+
     }
+
+    private void initEnergy(int i, int j) {
+        if (i == 0)
+            energy[i][j] = defaultEnergy;
+        else if (i == height() - 1)
+            energy[i][j] = defaultEnergy;
+        else if (j == 0)
+            energy[i][j] = defaultEnergy;
+        else if (j == width() - 1)
+            energy[i][j] = defaultEnergy;
+        else {
+            Color left = pic.get(j - 1, i);
+            Color right = pic.get(j + 1, i);
+            Color up = pic.get(j, i - 1);
+            Color down = pic.get(j, i + 1);
+            double deltaX = Math.pow(right.getRed() - left.getRed(), 2) + Math.pow(
+                    right.getGreen() - left.getGreen(), 2) + Math.pow(
+                    right.getBlue() - left.getBlue(), 2);
+            double deltaY = Math.pow(up.getRed() - down.getRed(), 2) + Math.pow(
+                    up.getGreen() - down.getGreen(), 2) + Math.pow(
+                    up.getBlue() - down.getBlue(), 2);
+
+            energy[i][j] = Math.sqrt(deltaX + deltaY);
+        }
+    }
+
 
     // current picture
     public Picture picture() {
-        return null;
+        return pic;
     }
 
     // width of current picture
     public int width() {
-        return 0;
+        return pic.width();
     }
 
     // height of current picture
     public int height() {
-        return 0;
+        return pic.height();
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        return 0;
+        return energy[y][x];
     }
 
     // sequence of indices for horizontal seam
--- seam/SeamCarverTest.java
+++ seam/SeamCarverTest.java
@@ -10,8 +10,8 @@ class SeamCarverTest {
         Picture picture = new Picture("3x4.png");
         SeamCarver sc = new SeamCarver(picture);
 
-        assertEquals(Math.sqrt(52024), sc.energy(1, 2), 0.001);
-        assertEquals(Math.sqrt(52225), sc.energy(1, 1), 0.001);
+        assertEquals(Math.sqrt(52225), sc.energy(1, 1), 0.00001);
+        assertEquals(Math.sqrt(52024), sc.energy(1, 2), 0.00001);
 
         // boarder should be 1000
         assertEquals(1000, sc.energy(0, 0));
