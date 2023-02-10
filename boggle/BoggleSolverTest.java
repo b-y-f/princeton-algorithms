a07b4b8c9d2a007e37a754634285748a34ff2ec4
Yifan <bbbbb8bbbbb@gmail.com>
2023-02-10 16:45:21 +1300

    Add test couscous

--- boggle/BoggleSolverTest.java
+++ boggle/BoggleSolverTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  **************************************************************************** */
 class BoggleSolverTest {
     private In in = new In("dictionary-algs4.txt");
-    private String[] dictionary = getIn().readAllStrings();
+    private String[] dictionary = in.readAllStrings();
     private BoggleSolver solver = new BoggleSolver(dictionary);
 
     @Test
@@ -24,6 +24,17 @@ class BoggleSolverTest {
     }
 
     @Test
+    public void testBoardCouscous() {
+        BoggleBoard board = new BoggleBoard("board-couscous.txt");
+        int score = 0;
+        for (String word : solver.getAllValidWords(board)) {
+            System.out.println(word);
+            score += solver.scoreOf(word);
+        }
+        assertEquals(24, score);
+    }
+
+    @Test
     public void testBoardQ() {
         BoggleBoard boardQ = new BoggleBoard("board-q.txt");
         int score = 0;
@@ -32,23 +43,4 @@ class BoggleSolverTest {
         }
         assertEquals(84, score);
     }
-
-    public In getIn() {
-        return in;
-    }
-
-    public void setIn(In in) {
-        this.in = in;
-    }
-
-    // @Test
-    // public void testBoardCouscous() {
-    //     BoggleBoard board = new BoggleBoard("board-couscous.txt");
-    //     int cnt = 0;
-    //     for (String word : solver.getAllValidWords(board)) {
-    //         cnt++;
-    //     }
-    //     assertEquals(12, cnt);
-    //
-    // }
 }
