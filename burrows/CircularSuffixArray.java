689bd412c51d9287d73c5e22d9fd0008f3e0f550
Yifan <bbbbb8bbbbb@gmail.com>
2023-02-11 17:09:20 +1300

    Add apis

---
+++ burrows/CircularSuffixArray.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class CircularSuffixArray {
+
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+    }
+
+    public int length() {
+        return 0;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        return i;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+    }
+
+}
