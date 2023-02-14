726148eed1f05512ef46d1b28e5397f867f9c9a3
Yifan <bbbbb8bbbbb@gmail.com>
2023-02-14 20:58:14 +1300

    reformat

--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -8,31 +8,25 @@ import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.Stack;
 
-// TODO You must use either SET or java.util.TreeSet; do not implement your own red–black BST.
 public class PointSET {
     private SET<Point2D> points;
 
-    public PointSET()
-    // construct an empty set of points
-    {
+    /**
+     * construct an empty set of points
+     */
+    public PointSET() {
         points = new SET<>();
     }
 
-    public boolean isEmpty()
-    // is the set empty?
-    {
+    public boolean isEmpty() {
         return points.isEmpty();
     }
 
-    public int size()
-    // number of points in the set
-    {
+    public int size() {
         return points.size();
     }
 
-    public void insert(Point2D p)
-    // add the point to the set (if it is not already in the set)
-    {
+    public void insert(Point2D p) {
         if (p == null) {
             throw new IllegalArgumentException();
         }
@@ -41,27 +35,21 @@ public class PointSET {
         }
     }
 
-    public boolean contains(Point2D p)
-    // does the set contain point p?
-    {
+    public boolean contains(Point2D p) {
         if (p == null) {
             throw new IllegalArgumentException();
         }
         return points.contains(p);
     }
 
-    public void draw()
-    // draw all points to standard draw
-    {
+    public void draw() {
         for (Point2D p : points) {
             p.draw();
         }
 
     }
 
-    public Iterable<Point2D> range(RectHV rect)
-    // all points that are inside the rectangle (or on the boundary)
-    {
+    public Iterable<Point2D> range(RectHV rect) {
         if (rect == null) {
             throw new IllegalArgumentException();
         }
@@ -74,9 +62,7 @@ public class PointSET {
         return insiders;
     }
 
-    public Point2D nearest(Point2D p)
-    // a nearest neighbor in the set to point p; null if the set is empty
-    {
+    public Point2D nearest(Point2D p) {
         if (p == null) {
             throw new IllegalArgumentException();
         }
@@ -89,8 +75,5 @@ public class PointSET {
             }
         }
         return nearest;
-    }
-
-    public static void main(String[] args) {
     }
 }
