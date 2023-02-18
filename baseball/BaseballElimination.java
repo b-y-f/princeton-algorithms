eb147d28c904f443bdd5f2e4299799f90a6b3dcf
Yifan <bbbbb8bbbbb@gmail.com>
2023-02-18 16:05:10 +1300

    add other api, test in main

--- baseball/BaseballElimination.java
+++ baseball/BaseballElimination.java
@@ -46,8 +46,13 @@ public class BaseballElimination {
     }
 
     public Iterable<String> teams() {
-        return null;
+        Stack<String> iter = new Stack<>();
+        for (String t : teams) {
+            iter.push(t);
+        }
+        return iter;
     }
+
 
     public int wins(String team) {
         int index = getTeamIndex(team);
@@ -85,6 +90,19 @@ public class BaseballElimination {
     }
 
     public static void main(String[] args) {
-
+        BaseballElimination division = new BaseballElimination(args[0]);
+        for (String team : division.teams()) {
+            if (division.isEliminated(team)) {
+                StdOut.print(team + " is eliminated by the subset R = { ");
+                for (String t : division.certificateOfElimination(team)) {
+                    StdOut.print(t + " ");
+                }
+                StdOut.println("}");
+            }
+            else {
+                StdOut.println(team + " is not eliminated");
+            }
+        }
     }
+
 }
